3a5b22dafbfc8ecdb5ebccebd18e9b66b8d1d225
Gaurav31U <gaurav319288@gmail.com>
2024-07-27 00:02:28 +0530

    Added client and server

---
+++ client.py
@@ -0,0 +1,32 @@
+import threading
+import socket
+username = input('Choose an username >>> ')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55524))
+
+
+def client_receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "username?":
+                client.send(username.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('Error! Server not connected')
+            client.close()
+            break
+
+
+def client_send():
+    while True:
+        message = f'{username}: {input("")}'
+        client.send(message.encode('utf-8'))
+
+
+receive_thread = threading.Thread(target=client_receive)
+receive_thread.start()
+
+send_thread = threading.Thread(target=client_send)
+send_thread.start()
